ef8b7b4e9eac12aa8b4cc5b6b5d41335879a1aef
Cest <cestrmail@163.com>
2018-04-03 15:24:12 +0800

    # 新增header模块

--- src/ershouche/index.js
+++ src/ershouche/index.js
@@ -1,4 +1,12 @@
 import './../../widget/base/index.scss'
+import './../../widget/header/index.scss'
 import './../../widget/filter/index.scss'
 import './../../widget/infolist/index.scss'
 import './index.scss'
+
+async function testAsync() {
+    return "hello async";
+}
+
+const result = testAsync();
+console.log(result);

4a3a7bda3acebf79379bef6ddc231ecb84a41f6f
Cest <cestrmail@163.com>
2018-04-04 08:41:07 +0800

    # 编译测试

---
+++ src/ershouche/b.js
@@ -0,0 +1,7 @@
+'use strict';
+let sex = "boy";
+let echo = function (value) {
+    alert(value + 'inner')
+};
+
+export {sex, echo};
--- src/ershouche/index.js
+++ src/ershouche/index.js
@@ -1,12 +1,16 @@
+'use strict';
 import './../../widget/base/index.scss'
 import './../../widget/header/index.scss'
 import './../../widget/filter/index.scss'
 import './../../widget/infolist/index.scss'
-import './index.scss'
+import   './index.scss'
 
-async function testAsync() {
-    return "hello async";
-}
 
-const result = testAsync();
-console.log(result);
+import md5 from 'js-md5'
+import {echo, sex} from "./b";
+
+const PI = 3.14;
+alert(PI);
+alert(md5(''));
+echo(sex);
+alert(sex);

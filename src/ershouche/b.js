e5444c68b5f74f68e977c5b3de3887070640dee0
Cest <cestrmail@163.com>
2018-04-08 12:02:48 +0800

    # 更新代码

--- src/ershouche/b.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-let sex = "boy";
-let echo = function (value) {
-    alert(value + 'inner')
-};
-
-export {sex, echo};
--- src/ershouche/index.js
+++ src/ershouche/index.js
@@ -3,14 +3,54 @@ import './../../widget/base/index.scss'
 import './../../widget/header/index.scss'
 import './../../widget/filter/index.scss'
 import './../../widget/infolist/index.scss'
-import   './index.scss'
+import './../../asset/iconfont/iconfont.css'
+import './index.scss'
 
+import $ from './../../asset/js/jquery-3.2.1.min'
 
-import md5 from 'js-md5'
-import {echo, sex} from "./b";
+let Page = function () {
+    let isAddStickyStyle = false;
+    let isRemoveStickyStyle = false;
+    let _page = {
+        init: function () {
+            this.onScorll();
+            this.setSticky();
+        },
+        setSticky: function () {
+            let scrollTop = $(document).scrollTop();
+            if (scrollTop > 99) {
+                if (!isAddStickyStyle) {
+                    isAddStickyStyle = true;
+                    isRemoveStickyStyle = false;
+                    $('.js_container').addClass('filter-fixed');
+                    let _animate = $('.js_container').find('.js_filter');
+                    _animate.addClass('animate');
+                    setTimeout(function () {
+                        _animate.removeClass('animate');
+                    }, 300);
+                }
+            } else {
+                if (!isRemoveStickyStyle) {
+                    $('.js_container').removeClass('filter-fixed');
+                    isAddStickyStyle = false;
+                    isRemoveStickyStyle = true;
+                }
+            }
+        },
+        onScorll: function () {
+            var self = this;
+            $(document).scroll(function (e) {
+                self.setSticky();
+            });
+        }
 
-const PI = 3.14;
-alert(PI);
-alert(md5(''));
-echo(sex);
-alert(sex);
+    };
+
+    return {
+        init: _page.init.bind(_page)
+    }
+}();
+
+$(function () {
+    Page.init();
+});
